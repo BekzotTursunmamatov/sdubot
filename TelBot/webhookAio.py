cbf39316727e8e74f2e50975f154391538ec2449
Bekzot <150104018@stu.sdu.edu.kz>
2018-05-30 00:11:15 +0600

    second update

--- TelBot/webhookAio.py
+++ TelBot/webhookAio.py
@@ -18,8 +18,8 @@ WEBHOOK_HOST = '<46.101.172.206'
 WEBHOOK_PORT = 8443  # 443, 80, 88 or 8443 (port need to be 'open')
 WEBHOOK_LISTEN = '0.0.0.0'  # In some VPS you may need to put here the IP addr
 
-WEBHOOK_SSL_CERT = 'C:/OpenSSL-Win32/bin/webhook_cert.pem'  # Path to the ssl certificate
-WEBHOOK_SSL_PRIV = 'C:/OpenSSL-Win32/bin/webhook_pkey.pem'  # Path to the ssl private key
+WEBHOOK_SSL_CERT = './webhook_cert.pem'  # Path to the ssl certificate
+WEBHOOK_SSL_PRIV = './webhook_pkey.pem'  # Path to the ssl private key
 
 # Quick'n'dirty SSL certificate generation:
 #
